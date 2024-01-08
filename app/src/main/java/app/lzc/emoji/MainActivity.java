fc8a9a0e583d49d4e0273e7fb90dbb45e5c2a41b
Laurence Luo <admin@lzc.app>
2024-01-08 18:11:25 +0800

    Update Emoji checking method

--- app/src/main/java/app/lzc/emoji/MainActivity.java
+++ app/src/main/java/app/lzc/emoji/MainActivity.java
@@ -1,33 +1,38 @@
 package app.lzc.emoji;
 
 import android.annotation.SuppressLint;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.TreeMap;
 
 public class MainActivity extends AppCompatActivity {
+    private String currentVersion;
+
+    private ArrayList<String[]> map;
 
     private int numValid = 0, numMax = 0;
 
@@ -38,65 +43,83 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView title = MainActivity.this.findViewById(R.id.title);
         title.setText(getString(R.string.ui_title));
+        PackageInfo packageInfo;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        TextView appver = MainActivity.this.findViewById(R.id.textView6);
+        appver.setText(packageInfo.versionName);
         new Thread(() -> {
             try {
                 testUnicode();
-
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
+            } catch (IOException | InterruptedException | JSONException e) {
+                //e.printStackTrace();
+                Log.e("Error", e.toString());
             }
         }).start();
     }
 
-    private void testUnicode() throws IOException, InterruptedException {
+    @SuppressLint("SetTextI18n")
+    private void testUnicode() throws IOException, InterruptedException, JSONException {
         File path = getApplication().getFilesDir();
-        String version = "";
-        try {
-            URL urlVersion = new URL("https://app.lzc.app/emoji/version");
-            HttpURLConnection connect = (HttpURLConnection) urlVersion.openConnection();
-            InputStream input = connect.getInputStream();
-            BufferedReader in = new BufferedReader(new InputStreamReader(input));
-            String line;
-            System.out.println(connect.getResponseCode());
-            StringBuilder sb = new StringBuilder();
-            while ((line = in.readLine()) != null) {
-                sb.append(line);
-            }
-            version = sb.toString();
 
-            if (Double.parseDouble(version) > 14.0) {
-                URL url = new URL("https://app.lzc.app/emoji/emoji-test.txt");
-                URLConnection connection = url.openConnection();
-                connection.connect();
-                InputStream is = connection.getInputStream();
-                FileOutputStream fos = new FileOutputStream(path + "/" + "emoji-test.txt");
-                byte[] buffer = new byte[1024];
-                do {
-                    int len = is.read(buffer);
-                    if (len == -1) {
-                        break;
-                    }
-                    fos.write(buffer, 0, len);
-                } while (true);
-                is.close();
-                fos.close();
+        URL urlVersion = new URL("https://app.lzc.app/emoji/version");
+        HttpURLConnection connect = (HttpURLConnection) urlVersion.openConnection();
+        InputStream input = connect.getInputStream();
+        BufferedReader in = new BufferedReader(new InputStreamReader(input));
+        String str;
+        System.out.println(connect.getResponseCode());
+        StringBuilder sbVersion = new StringBuilder();
+        while ((str = in.readLine()) != null) {
+            sbVersion.append(str);
+        }
+        currentVersion = sbVersion.toString();
+
+        URL urlEmojiTest = new URL("https://app.lzc.app/emoji/emoji-test.txt");
+        URLConnection connection = urlEmojiTest.openConnection();
+        connection.connect();
+        InputStream is = connection.getInputStream();
+        FileOutputStream fos = new FileOutputStream(path + "/" + "emoji-test.txt");
+        byte[] buffer = new byte[1024];
+        do {
+            int len = is.read(buffer);
+            if (len == -1) {
+                break;
             }
-        } catch (Exception ignored) {}
+            fos.write(buffer, 0, len);
+        } while (true);
+        is.close();
+        fos.close();
+
+        ArrayList<String[]> map = new ArrayList<>();
+        URL url = new URL("https://app.lzc.app/emoji/emoji.txt");
+        URLConnection connectionE = url.openConnection();
+        connectionE.connect();
+        BufferedReader br = new BufferedReader(new InputStreamReader(connectionE.getInputStream()));
+        StringBuilder sb = new StringBuilder();
+        String emojiLine;
+        while ((emojiLine = br.readLine()) != null) {
+            sb.append(emojiLine).append("\n");
+        }
+        br.close();
+        JSONObject obj = new JSONObject(sb.toString());
+        Iterator<String> keys = obj.keys();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            String value = obj.get(key).toString();
+            map.add(new String[]{key, value});
+        }
 
         InputStreamReader inputReader;
         File file = new File(path + "/" + "emoji-test.txt");
-        if (file.exists()) {
-            //noinspection IOStreamConstructor
-            inputReader = new InputStreamReader(new FileInputStream(file));
-        } else {
-            inputReader = new InputStreamReader(getResources().getAssets().open("emoji-test.txt"));
-        }
-
+        inputReader = new InputStreamReader(Files.newInputStream(file.toPath()));
 
         BufferedReader bufReader = new BufferedReader(inputReader);
         String line;
         while ((line = bufReader.readLine()) != null) {
-            if (line.startsWith("#") || line.equals("") || line.contains("minimally-qualified") || line.contains("unqualified")) {
+            if (line.startsWith("#") || line.isEmpty() || line.contains("minimally-qualified") || line.contains("unqualified")) {
                 continue;
             }
             String finalLine = line;
@@ -112,50 +135,22 @@ public class MainActivity extends AppCompatActivity {
             textView.setVisibility(View.GONE);
             textView3.setVisibility(View.GONE);
             progressBar.setVisibility(View.GONE);
-        });
-        checkVersion(version);
-    }
 
-    @SuppressLint("SetTextI18n")
-    private void checkVersion(String latest) {
-        runOnUiThread(() -> {
-            String emoji = "";
-            TreeMap<String, String> map = new TreeMap<>();
-            try {
-                URL url = new URL("https://app.lzc.app/emoji/emoji.txt");
-                URLConnection connection = url.openConnection();
-                connection.connect();
-                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                StringBuilder sb = new StringBuilder();
-                String line;
-                while ((line = br.readLine()) != null) {
-                    sb.append(line).append("\n");
-                }
-                br.close();
-                emoji = sb.toString();
+            String emoji;
 
-                JSONObject jobj = new JSONObject(emoji);
-                Iterator<String> keys = jobj.keys();
-                while(keys.hasNext()) {
-                    String key = keys.next();
-                    String value = jobj.get(key).toString();
-                    map.put(key, value);
-                }
-            } catch (Exception ignored) {}
-
-            String version = "";
-            for (TreeMap.Entry<String, String> entry : map.entrySet()) {
-                String key = entry.getKey();
-                String value = entry.getValue();
+            String emojiVersion = "";
+            for (String[] entry : map) {
+                String key = entry[0];
+                String value = entry[1];
                 if (validEmoji(value)) {
-                    version = key;
+                    emojiVersion = key;
                     break;
                 }
             }
 
             TextView textView4 = MainActivity.this.findViewById(R.id.textView4);
             textView4.setVisibility(View.VISIBLE);
-            textView4.setText("≈  Emoji " + version + "/" + latest);
+            textView4.setText("≈  Emoji " + emojiVersion + "/" + currentVersion);
         });
     }
 
